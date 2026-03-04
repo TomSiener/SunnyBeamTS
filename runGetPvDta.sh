8a4ca56853d93d051a93daac63c03e052cc80fa5
Tom <tsiener@gmx.de>
2026-03-04 13:54:10 +0100

    Update runGetPvDta.sh

---
+++ runGetPvDta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Zeitstempel im Format: JAHR-MONAT-TAG_STUNDE-MINUTE-SEKUNDE
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+LOGFILE="getPvData_${TIMESTAMP}.log"
+ERRFILE="getPvData_err_${TIMESTAMP}.log"
+
+#  stdout in LOGFILE und stderr in beide files
+echo "activate python env" 1>> "$LOGFILE" 2> >(tee -a "$LOGFILE" > "$ERRFILE")
+source /home/tom/.env/bin/activate 1>> "$LOGFILE" 2> >(tee -a "$LOGFILE" > "$ERRFILE")
+
+echo "run getPvData" 1>> "$LOGFILE" 2> >(tee -a "$LOGFILE" > "$ERRFILE")
+python ./getPvData.py 1>> "$LOGFILE" 2> >(tee -a "$LOGFILE" > "$ERRFILE")
