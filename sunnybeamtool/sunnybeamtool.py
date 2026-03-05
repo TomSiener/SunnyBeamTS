66cec11cae7e0d4abfadefe3967f3f98e903bd26
Tom <tsiener@gmx.de>
2026-03-05 10:36:23 +0100

    Update sunnybeamtool.py

--- sunnybeamtool/sunnybeamtool.py
+++ sunnybeamtool/sunnybeamtool.py
@@ -154,8 +154,7 @@ class SunnyBeam:
         await loop.run_in_executor(None, dev.reset)
         await loop.run_in_executor(None, dev.set_configuration)
         self._dev = dev
-         _LOGGER.info(
-                f"Connected to %s from %s with serial number %s" % (self._dev.product,self._dev.manufacturer,self._dev.serial_number,))
+        _LOGGER.info("Connected to %s from %s with serial number %s" % (self._dev.product,self._dev.manufacturer,self._dev.serial_number,))
         
         #pdb.set_trace()  #zum debuggen
         
