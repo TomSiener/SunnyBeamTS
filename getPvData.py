5f8a33029f2647b07e730d5c97678efea7614f87
Tom <tsiener@gmx.de>
2026-03-10 17:33:54 +0100

    Update getPvData.py
    unkorrigerte GesamtPV

--- getPvData.py
+++ getPvData.py
@@ -149,6 +149,7 @@ async def main():
                 client_mqtt_local.publish(MQTT_TOPIC + "power" , -float(data.get("power"))) #negativ
                 client_mqtt_local.publish(MQTT_TOPIC + "energy_today" , int(data.get("energy_today"))*1000) #WH
                 client_mqtt_local.publish(MQTT_TOPIC + "energy_total" , float(data.get("energy_total")+ENERGY_TOTAL_KORR)*1000)  #WH
+                client_mqtt_local.publish(MQTT_TOPIC + "energy_total_uncorr" , float(data.get("energy_total"))*1000)  #WH
                 client_mqtt_local.publish(MQTT_TOPIC + "update_time" , jetzt)
                 status_msg += "local MQTT-Werte gesendet. "
 
