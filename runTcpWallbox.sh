71d0ddbb94cf1626c2c3030b2b7c83240ef05d37
Tom <tsiener@gmx.de>
2026-03-26 15:37:20 +0100

    copy fronius for wallbox

---
+++ runTcpWallbox.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Zeitstempel im Format: JAHR-MONAT-TAG_STUNDE-MINUTE-SEKUNDE
+TIMESTAMP=$(date +"%Y%m%d-%H-%M-%S")
+
+#LOGFILE="server_pymodbus1-${TIMESTAMP}.log"
+LOGFILE="/dev/null"
+echo "Logfile $LOGFILE"
+
+cd /home/tom/SunnyBeamTS
+
+#stdout und stderr in LOGFILE und console
+echo "activate python env" #2>&1 | tee $LOGFILE
+
+#source funktioniert mit redirect nicht mehr
+source /home/tom/.env/bin/activate #2>&1 | tee $LOGFILE
+pip -V
+
+echo "run server_pymodbus1.py" #2>&1 | tee $LOGFILE
+python3 ./froniussimulator_3_noCheckMK.py 2>&1 | tee $LOGFILE
+#python3 ./server_pymodbus1.py --comm tcp --port 502
+#2>&1 | tee $LOGFILE
+echo "after server_pymodbus1.py" #2>&1 | tee $LOGFILE
